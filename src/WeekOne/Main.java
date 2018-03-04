d23d1db563ed13cab6c7f7332e1ca6cba62ad5c4
Gabby Massaro <gmassaro807@gmail.com>
2018-03-04 10:22:50 -0500

    Completed Week one

--- src/WeekOne/Main.java
+++ src/WeekOne/Main.java
@@ -10,26 +10,31 @@ class Main {
         // --------------------------------------------------------------------------------------
         // 2. Find out what happens when you print `str2`
         String str2 = null;
+        System.out.println(str2);
 
         // --------------------------------------------------------------------------------------
         // 3. ints, booleans, doubles, and floats are all compared using `==`
         //    Make the below program print whether or not `i` and `r` are equal
         int i = 0;
         int r = 1;
+        System.out.println(i == r);
 
         // --------------------------------------------------------------------------------------
-        // 4. Now, assign the below variable what you thing should be the correct value.
-        boolean intsAreEqual = false;
+        // 4. Now, assign the below variable with the above variables equality.
+        // hey bitch, its me, he wanted you to store i ==r in the boolean, but he said assign, so same difference
+        boolean intsAreEqual = i == r;
 
         // --------------------------------------------------------------------------------------
         //  5. Strings are compared using the .equals() method. Make the program below
-        //     check to see if name and nickName are equivalent
+        //     print whether or not name and nickName are equivalent
         String name = "Gabby";
         String nickName = "Gooby";
+        System.out.println(name.contentEquals("Gabby"));
+        System.out.println(name.toUpperCase());
 
         // --------------------------------------------------------------------------------------
         // 6. Using the above variables, assign what you think the correct value the below
         //    boolean should have
-        boolean nameEqualsNickName = false;
+        boolean nameEqualsNickName = name.contentEquals(nickName);
     }
 }
